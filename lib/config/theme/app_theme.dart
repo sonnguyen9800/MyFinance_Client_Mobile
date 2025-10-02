a265269dd24a01a295a9f8bb1742ad0d88fd622b
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2025-10-02 23:34:59 +0700

    + fix

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -34,7 +34,7 @@ class AppTheme {
       ),
 
       // Card Theme
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         elevation: 2,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(12),
@@ -142,7 +142,7 @@ class AppTheme {
       ),
 
       // Card Theme
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         elevation: 4,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(12),
